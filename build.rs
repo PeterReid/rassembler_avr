dcd4b04aa44b9d17fb012c3fbea4f54812395ea8
Peter Reid <peter.d.reid@gmail.com>
2017-03-10 23:08:33 -0500

    Initial commit. Largely untested, but all instructions are there

---
+++ build.rs
@@ -0,0 +1,225 @@
+
+use std::env;
+use std::path::Path;
+use std::fs::File;
+use std::io::Write;
+
+type InstructionSpec = (&'static str, &'static [Arg], &'static str);
+
+#[derive(Copy, Clone, Debug)]
+enum Arg {
+    Register(char),
+    RegisterPair(char),
+    Unsigned(char),
+    RelativeOffset(char),
+    AbsoluteOffset(char),
+    ImplicitZ,
+    
+}
+
+impl Arg {
+    fn name(&self) -> String {
+        self.format_char().to_string().to_lowercase()
+    }
+    
+    fn format_char(&self) -> char {
+        match self { 
+            &Arg::Register(c) => c,
+            &Arg::RegisterPair(c) => c,
+            &Arg::Unsigned(c) => c,
+            &Arg::RelativeOffset(c) => c,
+            &Arg::AbsoluteOffset(c) => c,
+            &Arg::ImplicitZ => 'z',
+        }
+    }
+    
+    fn type_str(&self) -> String {
+        match self {
+            &Arg::Register(_) => "Register".to_string(),
+            &Arg::RegisterPair(_) => "RegisterPair".to_string(),
+            &Arg::Unsigned(_) => "u32".to_string(),
+            &Arg::RelativeOffset(_) => "Offset".to_string(),
+            &Arg::AbsoluteOffset(_) => "Offset".to_string(),
+            &Arg::ImplicitZ => "RegisterPair".to_string(),
+        }
+    }
+}
+
+const RD: Arg = Arg::Register('d');
+const RR: Arg = Arg::Register('r');
+const RD_PAIR: Arg = Arg::RegisterPair('d');
+const RR_PAIR: Arg = Arg::RegisterPair('r');
+const K: Arg = Arg::Unsigned('K');
+const S: Arg = Arg::Unsigned('s');
+const B: Arg = Arg::Unsigned('b');
+const OFFSET: Arg = Arg::RelativeOffset('k');
+const ABSOLUTE_OFFSET: Arg = Arg::AbsoluteOffset('k');
+const A: Arg = Arg::Unsigned('A');
+
+static INSTRUCTIONS: [InstructionSpec; 117] = [
+    ("adc", &[RD, RR], "0001 11rd dddd rrrr"),
+    ("add", &[RD, RR], "0000 11rd dddd rrrr"),
+    ("adiw", &[RD_PAIR, K], "1001 0110 KKdd KKKK"),
+    ("and", &[RD, RR], "0010 00rd dddd rrrr"),
+    ("andi", &[RD, K], "0111 KKKK dddd KKKK"),
+    ("asr", &[RD], "1001 010d dddd 0101"),
+    ("bclr", &[S], "1001 0100 1sss 1000"), // TODO: Tests?
+    ("bld", &[RD, B], "1111 100d dddd 0bbb"),
+
+    ("brbc", &[S, OFFSET], "1111 01kk kkkk ksss"),
+    ("brbs", &[S, OFFSET], "1111 00kk kkkk ksss"),
+    ("brcc", &[OFFSET], "1111 01kk kkkk k000"),
+    ("brcs", &[OFFSET], "1111 00kk kkkk k000"),
+    ("break_", &[], "1001 0101 1001 1000"),
+    ("breq", &[OFFSET], "1111 00kk kkkk k001"),
+    ("brge", &[OFFSET], "1111 01kk kkkk k100"),
+    ("brhc", &[OFFSET], "1111 01kk kkkk k101"),
+    ("brhs", &[OFFSET], "1111 00kk kkkk k101"),
+    ("brid", &[OFFSET], "1111 01kk kkkk k111"),
+    ("brie", &[OFFSET], "1111 00kk kkkk k111"),
+    ("brlo", &[OFFSET], "1111 00kk kkkk k000"),
+    ("brlt", &[OFFSET], "1111 00kk kkkk k100"),
+    ("brmi", &[OFFSET], "1111 00kk kkkk k010"),
+    ("brne", &[OFFSET], "1111 01kk kkkk k001"),
+    ("brpl", &[OFFSET], "1111 01kk kkkk k010"),
+    ("brsh", &[OFFSET], "1111 01kk kkkk k000"),
+    ("brtc", &[OFFSET], "1111 01kk kkkk k110"),
+    ("brts", &[OFFSET], "1111 00kk kkkk k110"),
+    ("brvc", &[OFFSET], "1111 01kk kkkk k011"),
+    ("brvs", &[OFFSET], "1111 00kk kkkk k011"),
+
+    
+    ("bset", &[S], "1001 0100 0sss 1000"),
+    ("bst", &[RD, B], "1111 101d dddd 0bbb"),
+    ("call", &[OFFSET], "1001 010k kkkk 111k kkkk kkkk kkkk kkkk"),
+    ("cbi", &[A, B], "1001 1000 AAAA Abbb"),
+    ("cbr", &[RD, K], "0111 KKKK dddd KKKK"), // TODO: test this
+    ("clc", &[], "1001 0100 1000 1000"),
+    ("clh", &[], "1001 0100 1101 1000"),
+    ("cli", &[], "1001 0100 1111 1000"),
+    ("cln", &[], "1001 0100 1010 1000"),
+    ("clr", &[RD], "0010 01dd dddd dddd"), // EOR RD,RD
+    ("cls", &[], "1001 0100 1100 1000"),
+    ("clt", &[], "1001 0100 1110 1000"),
+    ("clv", &[], "1001 0100 1011 1000"),
+    ("clz", &[], "1001 0100 1001 1000"),
+    ("com", &[RD], "1001 010d dddd 0000"),
+    ("cp", &[RD, RR], "0001 01rd dddd rrrr"),
+    ("cpc", &[RD, RR], "0000 01rd dddd rrrr"),
+    ("cpi", &[RD, K], "0011 KKKK dddd KKKK"),
+    ("cpse", &[RD, RR], "0001 00rd dddd rrrr"),
+    ("dec", &[RD], "1001 010d dddd 1010"),
+    ("des", &[K], "1001 0100 KKKK 1011"),
+    ("eicall", &[], "1001 0101 0001 1001"),
+    ("eijmp", &[], "1001 0100 0001 1001"),
+    ("elpm_r0", &[], "1001 0101 1101 1000"),
+    ("eor", &[RD, RR], "0010 01rd dddd rrrr"),
+    ("fmul", &[RD, RR], "0000 0011 0ddd 1rrr"),
+    ("fmuls", &[RD, RR], "0000 0011 1ddd 0rrr"),
+    ("fmulsu", &[RD, RR], "0000 0011 1ddd 1rrr"),
+    ("icall", &[], "1001 0101 0000 1001"),
+    ("ijmp", &[], "1001 0100 0000 1001"),
+    ("in_", &[RD, A], "1011 0AAd dddd AAAA"),
+    ("inc", &[RD], "1001 010d dddd 0011"),
+    ("jmp", &[OFFSET], "1001 010k kkkk 110k kkkk kkkk kkkk kkkk"),
+    ("lac", &[Arg::ImplicitZ, RD], "1001 001d dddd 0110"),
+    ("las", &[Arg::ImplicitZ, RD], "1001 001d dddd 0101"),
+    ("lat", &[Arg::ImplicitZ, RD], "1001 001d dddd 0111"),
+    ("ldi", &[RD,K], "1110 KKKK dddd KKKK"),
+    ("lds_16", &[RD,ABSOLUTE_OFFSET], "1001 000d dddd 0000 kkkk kkkk kkkk kkkk"),
+    ("lds_7", &[RD,ABSOLUTE_OFFSET], "1010 0kkk dddd kkkk"),
+    ("lpm_r0", &[], "1001 0101 1100 1000"),
+    ("lsl", &[RD], "0000 11dd dddd dddd"),
+    ("lsr", &[RD], "1001 010d dddd 0110"),
+    ("mov", &[RD, RR], "0010 11rd dddd rrrr"),
+    ("movw", &[RD_PAIR,RR_PAIR], "0000 0001 dddd rrrr"),
+    ("mul", &[RD, RR], "1001 11rd dddd rrrr"),
+    ("muls", &[RD, RR], "0000 0010 dddd rrrr"),
+    ("mulsu", &[RD, RR], "0000 0011 0ddd 0rrr"),
+    ("neg", &[RD], "1001 010d dddd 0001"),
+    ("nop", &[], "0000 0000 0000 0000"),
+    ("or", &[RD, RR], "0010 10rd dddd rrrr"),
+    ("ori", &[RD, K], "0110 KKKK dddd KKKK"),
+    ("out", &[A, RR], "1011 1AAr rrrr AAAA"),
+    ("pop", &[RD], "1001 000d dddd 1111"),
+    ("push", &[RR], "1001 001r rrrr 1111"),
+    ("rcall", &[OFFSET], "1101 kkkk kkkk kkkk"),
+    ("ret", &[], "1001 0101 0000 1000"),
+    ("reti", &[], "1001 0101 0001 1000"),
+    ("rjmp", &[OFFSET], "1100 kkkk kkkk kkkk"),
+    ("rol", &[RD], "0001 11dd dddd dddd"),
+    ("ror", &[RD], "1001 010d dddd 0111"),
+    ("sbc", &[RD, RR], "0000 10rd dddd rrrr"),
+    ("sbci", &[RD, K], "0100 KKKK dddd KKKK"),
+    ("sbi", &[A, B], "1001 1010 AAAA Abbb"),
+    ("sbic", &[A, B], "1001 1001 AAAA Abbb"),
+    ("sbis", &[A, B], "1001 1011 AAAA Abbb"),
+    ("sbiw", &[RD_PAIR, K], "1001 0111 KKdd KKKK"),
+    ("sbr", &[RD, K], "0110 KKKK dddd KKKK"),
+    ("sbrc", &[RR, B], "1111 110r rrrr 0bbb"),
+    ("sbrs", &[RR, B], "1111 111r rrrr 0bbb"),
+    ("sec", &[], "1001 0100 0000 1000"),
+    ("seh", &[], "1001 0100 0101 1000"),
+    ("sei", &[], "1001 0100 0111 1000"),
+    ("sen", &[], "1001 0100 0010 1000"),
+    ("ser", &[RD], "1110 1111 dddd 1111"),
+    ("ses", &[], "1001 0100 0100 1000"),
+    ("set", &[], "1001 0100 0110 1000"),
+    ("sev", &[], "1001 0100 0011 1000"),
+    ("sez", &[], "1001 0100 0001 1000"),
+    ("sleep", &[], "1001 0101 1000 1000"),
+    ("spm", &[], "1001 0101 1110 1000"),
+    ("spm_z_plus", &[], "1001 0101 1111 1000"),
+    ("sts", &[ABSOLUTE_OFFSET, RD], "1001 001d dddd 0000 kkkk kkkk kkkk kkkk"),
+    ("sub", &[RD, RR], "0001 10rd dddd rrrr"),
+    ("subi", &[RD, K], "0101 KKKK dddd KKKK"),
+    ("swap", &[RD], "1001 010d dddd 0010"),
+    ("tst", &[RD], "0010 00dd dddd dddd"),
+    ("wdr", &[], "1001 0101 1010 1000"),
+    ("xch", &[Arg::ImplicitZ, RD], "1001 001d dddd 0100"),
+];
+
+fn main() {
+    let mut lines: Vec<String> = Vec::new();
+    
+    lines.push("impl Assembler {".to_string());
+    
+    for &(name, args, template) in INSTRUCTIONS.iter() {
+        let arg_strs: Vec<String> = args.iter().map(
+            |arg| format!("{}: {}", arg.name(), arg.type_str())
+        ).collect();
+    
+        let arg_intos: Vec<_> = args.iter().map(
+            |arg| format!("({}.into(), b'{}')", arg.name(), arg.format_char())
+        ).collect();
+    
+        lines.push(format!("    pub fn {}(&mut self, {}) {{", name, arg_strs.join(", ")));
+        for arg in args.iter() {
+            match *arg {
+                Arg::ImplicitZ => {
+                    lines.push("        assert!(z == Z, \"Z (R30,30) is the only accepted argument\");".to_string());
+                }
+                Arg::RelativeOffset(_) => {
+                    lines.push(format!("        let {} = self.resolve_relative_offset({});", arg.name(), arg.name()));
+                }
+                Arg::AbsoluteOffset(_) => {
+                    lines.push(format!("        let {} = self.resolve_absolute_offset({});", arg.name(), arg.name()));
+                }
+                _ => {}
+            }
+        }
+        
+        lines.push(format!("        self.encode(&[{}][..], b{:?})", arg_intos.join(", "), template));
+        lines.push("    }".to_string());
+    }
+    
+    lines.push("}".to_string());
+    lines.push("".to_string());
+    
+    let text = lines.join("\n");
+    
+    let out_dir = env::var("OUT_DIR").expect("OUT_DIR environment variable not set");
+    let dest_path = Path::new(&out_dir).join("ops.rs");
+    
+    File::create(dest_path).and_then(|mut f| f.write_all(&text.as_bytes())).expect("writing ops.rs failed");
+}
---
+++ src/lib.rs
@@ -0,0 +1,379 @@
+// RPtrPair: R25:R24 or R27:R26 or R29:R28 or R31:R31
+// U6: Integer 0 <= x < 64
+// Rd: Destination register R0-R31
+// Rr: Register R0-R31
+
+
+use std::convert::Into;
+use std::ops::Add;
+
+
+pub struct Assembler {
+    pub buf: Vec<u8>
+}
+
+impl Assembler {
+    pub fn new() -> Assembler{
+        Assembler {
+            buf: Vec::new()
+        }
+    }
+}
+
+pub fn relative(x: i32) -> Offset {
+    assert!(x % 2 == 0);
+    Offset::Relative(x / 2)
+}
+pub fn absolute(x: u32) -> Offset {
+    Offset::Absolute(x)
+}
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct Register(u32);
+
+pub const R0: Register = Register(0);
+pub const R1: Register = Register(1);
+pub const R2: Register = Register(2);
+pub const R3: Register = Register(3);
+pub const R4: Register = Register(4);
+pub const R5: Register = Register(5);
+pub const R6: Register = Register(6);
+pub const R7: Register = Register(7);
+pub const R8: Register = Register(8);
+pub const R9: Register = Register(9);
+pub const R10: Register = Register(10);
+pub const R11: Register = Register(11);
+pub const R12: Register = Register(12);
+pub const R13: Register = Register(13);
+pub const R14: Register = Register(14);
+pub const R15: Register = Register(15);
+pub const R16: Register = Register(16);
+pub const R17: Register = Register(17);
+pub const R18: Register = Register(18);
+pub const R19: Register = Register(19);
+pub const R20: Register = Register(20);
+pub const R21: Register = Register(21);
+pub const R22: Register = Register(22);
+pub const R23: Register = Register(23);
+pub const R24: Register = Register(24);
+pub const R25: Register = Register(25);
+pub const R26: Register = Register(26);
+pub const R27: Register = Register(27);
+pub const R28: Register = Register(28);
+pub const R29: Register = Register(29);
+pub const R30: Register = Register(30);
+pub const R31: Register = Register(31);
+
+#[derive(Copy, Clone, Eq, PartialEq)]
+pub struct RegisterPair(pub Register, pub Register);
+
+pub const X: RegisterPair = RegisterPair(R27, R26);
+pub const Y: RegisterPair = RegisterPair(R29, R28);
+pub const Z: RegisterPair = RegisterPair(R31, R30);
+
+enum Direction {
+    PreDecrement,
+    NoChange,
+    PostIncrement,
+}
+
+pub struct DirectionalRegisterPair {
+    pair: RegisterPair,
+    direction: Direction,
+}
+
+
+pub struct OffsetRegisterPair {
+    pair: RegisterPair,
+    offset: u8,
+}
+
+
+impl RegisterPair {
+    pub fn post_increment(self) -> DirectionalRegisterPair {
+        DirectionalRegisterPair {
+            pair: self,
+            direction: Direction::PostIncrement,
+        }
+    }
+    pub fn pre_decrement(self) -> DirectionalRegisterPair {
+        DirectionalRegisterPair {
+            pair: self,
+            direction: Direction::PreDecrement,
+        }
+    }
+}
+
+impl Into<DirectionalRegisterPair> for RegisterPair {
+    fn into(self) -> DirectionalRegisterPair {
+        DirectionalRegisterPair{
+            pair: self,
+            direction: Direction::NoChange,
+        }
+    }
+}
+
+impl Add<u8> for RegisterPair {
+    type Output = OffsetRegisterPair;
+    
+    fn add(self, rhs: u8) -> OffsetRegisterPair {
+        OffsetRegisterPair{
+            pair: self,
+            offset: rhs,
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum Offset {
+    Absolute(u32),
+    Relative(i32),
+}
+
+#[derive(Copy, Clone)]
+enum Arg {
+    Register(Register),
+    RegisterPair(RegisterPair),
+    U32(u32),
+    U8(u8),
+    I32(i32),
+}
+
+impl Into<Arg> for Register {
+    fn into(self) -> Arg {
+        Arg::Register(self)
+    }
+}
+
+impl Into<Arg> for RegisterPair {
+    fn into(self) -> Arg {
+        Arg::RegisterPair(self)
+    }
+}
+
+impl From<u32> for Arg {
+    fn from(x: u32) -> Arg {
+        Arg::U32(x)
+    }
+}
+
+impl From<i32> for Arg {
+    fn from(x: i32) -> Arg {
+        Arg::I32(x)
+    }
+}
+impl From<u8> for Arg {
+    fn from(x: u8) -> Arg {
+        Arg::U8(x)
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+struct ArgConsumer {
+    value: u32
+}
+
+impl ArgConsumer {
+    fn new(arg: Arg, bit_count: usize) -> ArgConsumer{
+        ArgConsumer{
+            value: match arg {
+                Arg::Register(r) => {
+                    match bit_count {
+                        10 => {
+                            // This assumes a bit pattern xy xxxx yyyy
+                            assert!(r.0 < 32);
+                            (r.0 << 4) | ((r.0 & 16) << 9) | (r.0 & 0x0f)
+                        }
+                        5 => {
+                            assert!(r.0 < 32);
+                            r.0
+                        }
+                        4 => {
+                            assert!(16 <= r.0 && r.0 < 32);
+                            r.0 & 15
+                        }
+                        3 => {
+                            assert!(16 <= r.0 && r.0 < 24);
+                            r.0 & 7
+                        }
+                        _ => {
+                            panic!("unrecognized number of register bits: {}", bit_count);
+                        }
+                    }
+                },
+                Arg::RegisterPair(RegisterPair(high, low)) => {
+                    if high.0 != low.0+1 {
+                        panic!("Register pair must be consecutive HI,LO registers");
+                    }
+                    if low.0 % 2 == 1 {
+                        panic!("Low register in register pair must be an even-numbered register");
+                    }
+                    let min_representable = 32 - 2 * (1 << bit_count);
+                    if low.0 < min_representable {
+                        panic!("Register pair out of representable range. Must be at least R{}", min_representable);
+                    }
+                    low.0 / 2
+                }
+                Arg::U32(x) => {
+                    x
+                }
+                Arg::U8(x) => {
+                    x as u32
+                }
+                Arg::I32(x) => {
+                    // TODO: Validate that it fits in the big count?
+                    x as u32
+                }
+            }
+        }
+    }
+    
+    fn consume_bit(&mut self) -> u32 {
+        let ret = self.value & 1;
+        self.value = self.value >> 1;
+        //println!("consumed {}, has {} left", ret, self.value);
+        return ret;
+    }
+}
+
+impl Assembler {
+    fn encode(&mut self, args: &[(Arg, u8)], format: &[u8]) {
+        let mut occurrence_count = [0; 256];
+        for format_byte in format {
+            occurrence_count[*format_byte as usize] += 1;
+        }
+        
+        let mut arg_consumers: [Option<ArgConsumer>; 256] = [None; 256];
+        for &(arg, format_byte) in args {
+            arg_consumers[format_byte as usize] = Some(ArgConsumer::new(arg, occurrence_count[format_byte as usize]));
+        }
+        
+        let mut result: u32 = 0;
+        let mut result_bits: usize = 0;
+        
+        for format_byte in format.iter().rev() {
+            //println!("{}", format_byte);
+            let bit = match *format_byte {
+                b' ' => { continue },
+                b'0' => 0,
+                b'1' => 1,
+                ch => arg_consumers[ch as usize].as_mut().expect("Format character has no argument").consume_bit()
+            };
+            result = result | (bit << result_bits);
+            result_bits += 1;
+        }
+        
+        assert!(result_bits % 16 == 0);
+        while result_bits > 0 {
+            self.buf.push(((result >> (result_bits - 16)) & 0xff) as u8);
+            self.buf.push(((result >> (result_bits - 8)) & 0xff) as u8);
+            result_bits -= 16;
+        }
+    }
+    
+    fn resolve_absolute_offset(&self, offset: Offset) -> u32 {
+        match offset {
+            Offset::Absolute(x) => x,
+            Offset::Relative(x) => (self.buf.len() as i32 + x) as u32
+        }
+    }
+    fn resolve_relative_offset(&self, offset: Offset) -> i32 {
+        (self.resolve_absolute_offset(offset) as i32) - (self.buf.len() as i32)
+    }
+    
+    pub fn lds(&mut self, d: Register, k: Offset) {
+        let addr = self.resolve_absolute_offset(k);
+        //println!("resolved to {}", addr);
+        if addr < 128 {
+            self.lds_7(d, k);
+        } else {
+            self.lds_16(d, k);
+        }
+    }
+    
+    pub fn ld<R: Into<DirectionalRegisterPair>>(&mut self, d: Register, r: R) {
+        let r: DirectionalRegisterPair = r.into();
+        let template = match (r.pair, r.direction) {
+            (x, Direction::NoChange) if x == X => b"1001 000d dddd 1100",
+            (x, Direction::PostIncrement) if x == X => b"1001 000d dddd 1101",
+            (x, Direction::PreDecrement) if x == X => b"1001 000d dddd 1110",
+            (y, Direction::NoChange) if y == Y => b"1000 000d dddd 1000",
+            (y, Direction::PostIncrement) if y == Y => b"1001 000d dddd 1001",
+            (y, Direction::PreDecrement) if y == Y => b"1001 000d dddd 1010",
+            (z, Direction::NoChange) if z == Z => b"1000 000d dddd 0000",
+            (z, Direction::PostIncrement) if z == Z => b"1001 000d dddd 0001",
+            (z, Direction::PreDecrement) if z == Z => b"1001 000d dddd 0010",
+            _ => panic!("Invalid LD arguments")
+        };
+        
+        self.encode(&[(d.into(), b'd')][..], template)
+    }
+    
+    pub fn ldd(&mut self, d: Register, r: OffsetRegisterPair) {
+        let template = 
+            if r.pair == Y {
+                b"10q0 qq0d dddd 1qqq"
+            } else if r.pair == Z {
+                b"10q0 qq0d dddd 0qqq"
+            } else {
+                panic!("Invalid pointer for LDD");
+            };
+        self.encode(&[(d.into(), b'd'), (r.offset.into(), b'q')][..], template)
+    }
+    
+    pub fn st<D: Into<DirectionalRegisterPair>>(&mut self, d: D, r: Register) {
+        let d: DirectionalRegisterPair = d.into();
+        let template = match (d.pair, d.direction) {
+            (x, Direction::NoChange) if x == X => b"1001 001r rrrr 1100",
+            (x, Direction::PostIncrement) if x == X => b"1001 001r rrrr 1101",
+            (x, Direction::PreDecrement) if x == X => b"1001 001r rrrr 1110",
+            (y, Direction::NoChange) if y == Y => b"1000 001r rrrr 1000",
+            (y, Direction::PostIncrement) if y == Y => b"1001 001r rrrr 1001",
+            (y, Direction::PreDecrement) if y == Y => b"1001 001r rrrr 1010",
+            (z, Direction::NoChange) if z == Z => b"1000 001r rrrr 0000",
+            (z, Direction::PostIncrement) if z == Z => b"1001 001r rrrr 0001",
+            (z, Direction::PreDecrement) if z == Z => b"1001 001r rrrr 0010",
+            _ => panic!("Invalid ST arguments")
+        };
+        
+        self.encode(&[(r.into(), b'r')][..], template)
+    }
+    
+    pub fn std(&mut self, d: OffsetRegisterPair, r: Register) {
+        let template = 
+            if d.pair == Y {
+                b"10q0 qq1r rrrr 1qqq"
+            } else if d.pair == Z {
+                b"10q0 qq1r rrrr 0qqq"
+            } else {
+                panic!("Invalid pointer for STD");
+            };
+        self.encode(&[(r.into(), b'r'), (d.offset.into(), b'q')][..], template)
+    }
+    
+    pub fn lpm<R: Into<DirectionalRegisterPair>>(&mut self, d: Register, r: R) {
+        let r: DirectionalRegisterPair = r.into();
+        let template = match (r.pair, r.direction) {
+            (z, Direction::NoChange) if z == Z => b"1001 000d dddd 0100",
+            (z, Direction::PostIncrement) if z == Z => b"1001 000d dddd 0101",
+            _ => panic!("Invalid LPM arguments")
+        };
+        
+        self.encode(&[(d.into(), b'd')][..], template)
+    }
+    
+    pub fn elpm<R: Into<DirectionalRegisterPair>>(&mut self, d: Register, r: R) {
+        let r: DirectionalRegisterPair = r.into();
+        let template = match (r.pair, r.direction) {
+            (z, Direction::NoChange) if z == Z => b"1001 000d dddd 0110",
+            (z, Direction::PostIncrement) if z == Z => b"1001 000d dddd 0111",
+            _ => panic!("Invalid ELPM arguments")
+        };
+        
+        self.encode(&[(d.into(), b'd')][..], template)
+    }
+}
+
+
+include!(concat!(env!("OUT_DIR"), "/ops.rs"));
+
